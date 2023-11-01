60a46ecf29c94bf20930b9c2656b0b8da82775c1
miaowubo <15703216181@163.com>
2023-11-01 17:22:02 +0800

    再次测试8

--- log/build.gradle.kts
+++ log/build.gradle.kts
@@ -24,19 +24,11 @@ android {
         }
     }
     compileOptions {
-<<<<<<< HEAD
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "17"
-=======
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
->>>>>>> origin/main
     }
 }
 
